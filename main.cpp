f8d1ae77550ab82f40d0b7955340dc72c47aee02
VicentC0deFedora <vicenteestradomin@gmail.com>
2022-10-09 21:33:21 -0400

    My 1st program in c++

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int age = 0;
+    age = 18;
+    const char leter = 'V';
+    int ageLst[] = { 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30 };
+    ageLst[0] = 8;
+    cout << age << endl;
+    cout << leter << endl;
+    cout << ageLst[5] << endl;
+    cout << ageLst[0] << endl;
+    cout << "It is all" << endl;
+//    cout << "Hello Vicente!";
+}
